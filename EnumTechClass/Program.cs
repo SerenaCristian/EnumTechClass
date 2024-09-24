476067184ca0c413500236bd1e135f316368fc0d
SerenaCristian <devcristian02@gmail.com>
2024-09-24 19:08:06 -0300

    Atividade Enun 1.0

---
+++ EnumTechClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EnumTechClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = new string('-', 50);
+            Console.WriteLine(sep);
+            Console.WriteLine("Sistema Enum Tech Class");
+            Console.WriteLine(sep);
+
+            Motor MotorCarro = new Motor("Flex", 999, 1.0 );
+            Motor MotorCarroTiguan = new Motor("Flex", 1984, 2.0);
+
+            Veiculo uno = new Veiculo("Fiat", "uno", 2018, TipoVeiculo.Carro, MotorCarro);
+            Veiculo Tiguan = new Veiculo("Volkswagen", "SUV", 2022, TipoVeiculo.SUV, MotorCarroTiguan);
+
+            uno.ImprimirDetalhes();
+
+            Console.WriteLine(sep);
+
+            Tiguan.ImprimirDetalhes();
+
+            Console.WriteLine(sep);
+        }
+    }
+}
---
+++ EnumTechClass/Vehicle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace EnumTechClass
+{
+    public enum TipoVeiculo
+    {
+        Carro,
+        Caminhao,
+        Motocicleta,
+        SUV
+    }
+
+    public class Motor
+    {
+        public string TipoMotor { get; set; }
+        public int Potencia { get; set; }
+        public double Cilindrada { get; set; }
+
+        public Motor(string tipoMotor, int potencia, double cilindrada)
+        {
+            TipoMotor = tipoMotor;
+            Potencia = potencia;
+            Cilindrada = cilindrada;
+        }
+    }
+
+    internal class Veiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public TipoVeiculo Tipo { get; set; }
+        public Motor Motor { get; set; }
+
+        public Veiculo(string marca, string modelo, int ano, TipoVeiculo tipo, Motor motor)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Ano = ano;
+            Tipo = tipo;
+            Motor = motor;
+        }
+
+        public void ImprimirDetalhes()
+        {
+            Console.WriteLine("Detalhes do Veículo:");
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Ano: {Ano}");
+            Console.WriteLine($"Tipo: {Tipo}");
+            Console.WriteLine("Detalhes do Motor:");
+            Console.WriteLine($"  Tipo do Motor: {Motor.TipoMotor}");
+            Console.WriteLine($"  Potência: {Motor.Potencia} HP");
+            Console.WriteLine($"  Cilindrada: {Motor.Cilindrada} L");
+        }
+    }
+
+}
+
+
